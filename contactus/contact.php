f50ce83a42f7fe15a5b2d28845bb5e37a8ed34a5
Nandini551 <60439845+Nandini551@users.noreply.github.com>
2021-06-17 15:46:17 +0530

    Add files via upload
    contact us page added

---
+++ contactus/contact.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <title>Contact Us</title>
+  <link href="https://fonts.googleapis.com/css2?family=Jost:wght@200;300;400;500;600;700&display=swap" rel="stylesheet">
+
+<link href="//db.onlinewebfonts.com/c/95cecf452d3208890088a5b4c19c7ecf?family=Helvetica+Neue+ME" rel="stylesheet" type="text/css"/>
+<link rel="stylesheet" type="text/css" href="style.css">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+
+
+
+
+
+<div class="contact-container">
+  <div class="left-col">
+    <img class="logo" src=""/>
+<h1>Contact Us</h1>
+  </div>
+  <div class="right-col">
+    
+    <h1>Contact Us</h1>
+        <p>We promise to deliver on time.
+
+We deliver food and only good food.Freshness and craziness delivered at your doorstep.</p>
+    
+    
+    <form id="contact-form" method="post">
+      <label for="name">Full name</label>
+  <input type="text" id="name" name="name" placeholder="Your Full Name" required>
+      <label for="email">Email Address</label>
+  <input type="email" id="email" name="email" placeholder="Your Email Address" required>
+      <label for="message">Message</label>
+  <textarea rows="6" placeholder="Your Message" id="message" name="message" required></textarea>
+      <!--<a href="javascript:void(0)">--><button type="submit" id="submit" name="submit">Send</button><!--</a>-->
+  
+</form>
+<div id="error"></div>
+<div id="success-msg"></div>
+  </div>
+</div>
+
+<!-- Image credit: Oliver Sjöström https://www.pexels.com/photo/body-of-water-near-green-mountain-931018/  -->
