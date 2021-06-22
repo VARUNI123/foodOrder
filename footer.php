d47864665aebc4ddedd7cb3143814254fb080240
Nandini551 <60439845+Nandini551@users.noreply.github.com>
2021-06-22 17:48:07 +0530

    Update footer.php
    footer code changed

--- footer.php
+++ footer.php
@@ -1,33 +1,141 @@
-<footer>
-    <div class="column">
-        <a class="footer_title">TITLE</a>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. rum numquam ab impedit dolorem adipisci.</p>
-        <span><a href="#" title="Facebook"><i class="fa fa-facebook"></i></a></span>
-        <span><a href="#" title="Instagram"><i class="fa fa-instagram"></i></a></span>
-        <span><a href="#" title="Twitter"><i class="fa fa-twitter"></i></a></span>
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+<style type="text/css">
+  
+  .fas{
+  size:7x;
+}
+.container-footer{
+  max-width: 1170px;
+  margin:auto;
+}
+.row{
+  display: flex;
+  flex-wrap: wrap;
+}
+ul{
+  list-style: none;
+}
+.footer{
+  background-color: #24262b;
+    padding: 70px 0;
+  margin-top: 5%;
+}
+.footer-col{
+   width: 25%;
+   padding: 0 15px;
+}
+.footer-col h4{
+  font-size: 18px;
+  color: #ffffff;
+  text-transform: capitalize;
+  margin-bottom: 35px;
+  font-weight: 500;
+  position: relative;
+}
+.footer-col h4::before{
+  content: '';
+  position: absolute;
+  left:0;
+  bottom: -10px;
+  background-color: #e91e63;
+  height: 2px;
+  box-sizing: border-box;
+  width: 50px;
+}
+.footer-col ul li:not(:last-child){
+  margin-bottom: 10px;
+}
+.footer-col ul li a{
+  font-size: 16px;
+  text-transform: capitalize;
+  color: #ffffff;
+  text-decoration: none;
+  font-weight: 300;
+  color: #bbbbbb;
+  display: block;
+  transition: all 0.3s ease;
+}
+.footer-col ul li a:hover{
+  color: #ffffff;
+  padding-left: 8px;
+}
+.footer-col .social-links a{
+  display: inline-block;
+  height: 40px;
+  width: 40px;
+  background-color: rgba(255,255,255,0.2);
+  margin:0 10px 10px 0;
+  text-align: center;
+  line-height: 40px;
+  border-radius: 50%;
+  color: #ffffff;
+  transition: all 0.5s ease;
+}
+.footer-col .social-links a:hover{
+  color: #24262b;
+  background-color: #ffffff;
+}
 
-    </div>
-    <div class="column">
-        <a class="footer_title">LINKS</a>
-        <a href="#">Privacy Policy</a>
-        <a href="#">Terms &amp; Conditions</a>
-        <a href="#">Order</a>
-        <a href="#">Contact Us</a>
-    </div>
-    <div class="column">
-        <a class="footer_title">SHORT CUT</a>
-        <a href="#">Other Services</a>
-        <a href="#">Our Blog</a>
-        <a href="#">Our Projects</a>
-        <a href="#">About Us</a>
-    </div>
-    <div class="column">
-        <a class="footer_title">GET IN TOUCH</a>
-        <a href="#" title="address"><i class="fas fa-map-marker-alt"></i>&nbsp;15/658,street,area,state,country - zipcode</a>
-        <a href="#" title="Email"><i class="fa fa-envelope"></i>&nbsp;email@producername.com</a>
-        <a href="#" title="Contact"><i class="fa fas fa-phone"></i>&nbsp;9291347646</a>
-    </div>
-    <div class="sub-footer">
-        @CopyRights 2022 Title || All rights reserved
-    </div>
-</footer>
+/*responsive*/
+@media(max-width: 767px){
+  .footer-col{
+    width: 50%;
+    margin-bottom: 30px;
+}
+}
+@media(max-width: 574px){
+  .footer-col{
+    width: 100%;
+}
+}
+</style>
+</head>
+<body>
+<footer class="footer">
+     <div class="container-footer">
+      <div class="row">
+        <div class="footer-col">
+          <h4>company</h4>
+          <ul>
+            <li><a href="#">about us</a></li>
+            <li><a href="#">our services</a></li>
+            <li><a href="#">privacy policy</a></li>
+            <li><a href="#">affiliate program</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>get help</h4>
+          <ul>
+            <li><a href="#">FAQ</a></li>
+            <li><a href="#">shipping</a></li>
+            <li><a href="#">returns</a></li>
+            <li><a href="#">order status</a></li>
+            <li><a href="#">payment options</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>online shop</h4>
+          <ul>
+            <li><a href="#">gravies</a></li>
+            <li><a href="#">starters</a></li>
+            <li><a href="#">desserts</a></li>
+            <li><a href="#">non-veg</a></li>
+          </ul>
+        </div>
+        <div class="footer-col">
+          <h4>follow us</h4>
+          <div class="social-links">
+            <a href="#"><i class="fab fa-facebook-f"></i></a>
+            <a href="#"><i class="fab fa-twitter"></i></a>
+            <a href="#"><i class="fab fa-instagram"></i></a>
+            <a href="#"><i class="fab fa-linkedin-in"></i></a>
+          </div>
+        </div>
+      </div>
+     </div>
+  </footer>
+  </body>
+</html>
