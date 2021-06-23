afc3577279d896bf82d47b5b78c633f5cc8a4c8b
lalli <varunilalitha.bysani@gmail.com>
2021-06-23 12:57:38 +0530

    added quntity update feature

--- cartAdd.php
+++ cartAdd.php
@@ -6,26 +6,15 @@ if($auth!="")
 {
   $email = $_SESSION['email'];
 }
-if(isset($_GET['cartitem']))
+if(isset($_GET['cartitem']) && isset($_GET['quan']) && isset($_GET['cost']) && isset($_GET['img']) && isset($_GET['res']))
 {
   $item = $_GET['cartitem'];
-}
-if(isset($_GET['quan']))
-{
   $quan = $_GET['quan'];
-}
-if(isset($_GET['cost']))
-{
   $cost = $_GET['cost'];
-}
-if(isset($_GET['img']))
-{
   $img = $_GET['img'];
-}
-if(isset($_GET['res']))
-{
   $res = $_GET['res'];
 }
+
 
 $query1 = "SELECT * FROM `cartitems` WHERE `citem` = '$item' AND `email` = '$email'";
 if($qrun1 = mysqli_query($conn,$query1))
@@ -37,6 +26,9 @@ if($qrun1 = mysqli_query($conn,$query1))
     {
       echo "Please add the item quantity you want..!";
     }
+    else if($res==""){
+      echo "Please select the restaurant to add item into cart..!";
+    }
     else
     {
       $query2 = "INSERT INTO `cartitems` (email,citem,image,quan,cost,restaurant) VALUES ('$email','$item','$img',$quan,$cost,'$res')";
@@ -47,7 +39,12 @@ if($qrun1 = mysqli_query($conn,$query1))
     }
   }
   else{
-    echo '<span style="font-weight:bold;">'.$item.'</span> is already added to the cart..!';
+    $query3 = "UPDATE `cartitems` SET `quan`='$quan' WHERE `citem` = '$item'";
+    // echo '<span style="font-weight:bold;">'.$item.'</span> is already added to the cart..!';
+    if($qrun3 = mysqli_query($conn,$query3))
+      {
+        echo '<span style="font-weight:bold;">'.$item.'</span>quantity UPDATED in the cart successfully..!';
+      }
   }
 }
 else{
