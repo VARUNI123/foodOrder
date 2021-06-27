b19e465d4e009f030e5e7d67b5968b67b9d15891
Vamshi <vv517816@gmail.com>
2021-06-27 12:19:35 +0530

    Added billing form

--- fType.php
+++ fType.php
@@ -1,5 +1,10 @@
 <?php
 require('connect.php');
+require_once('googleLogin/config.php');
+if(!isset($_SESSION['access_token']))
+{
+  header('Location:http://localhost/fprjct/index.php');
+}
 $cat = $_GET['category'];
 $type = $_GET['ftype'];
 //echo "<h1>".$cat."</h1>";
